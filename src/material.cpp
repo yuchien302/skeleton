3fe9481cfc34093be156220b9ae62f72bff38b2a
yuchien <yc2257@cornell.edu>
2015-11-21 19:37:34 -0500

    Finish all materials constructor

--- src/material.cpp
+++ src/material.cpp
@@ -67,20 +67,6 @@ whitehdl::~whitehdl()
 
 }
 
-//<<<<<<< HEAD
-//vec3f whitehdl::shade_vertex(canvashdl *canvas, vec3f vertex, vec3f normal, vector<float> &varying) const
-//{
-//	// DONE Assignment 3: Move your solid color shader from the canvas to here.
-//	vec4f point = canvas -> matrices[canvas -> projection_matrix] * homogenize(vertex);
-//	point = point / point.data[3];
-//
-//	return point;
-//}
-//
-//vec3f whitehdl::shade_fragment(canvashdl *canvas, vector<float> &varying) const
-//{
-//	return vec3f(1.0, 1.0, 1.0);
-//=======
 void whitehdl::apply(const vector<lighthdl*> &lights)
 {
 	glUseProgram(program);
@@ -109,6 +95,13 @@ gouraudhdl::gouraudhdl()
 		 * this class. So you only have to initialize them once when the first instance of
 		 * the class is created.
 		 */
+		vertex = load_shader_file(working_directory + "res/gouraud.vx", GL_VERTEX_SHADER);
+		fragment = load_shader_file(working_directory + "res/gouraud.ft", GL_FRAGMENT_SHADER);
+		program = glCreateProgram();
+		glAttachShader(program, vertex);
+		glAttachShader(program, fragment);
+		glLinkProgram(program);
+
 	}
 }
 
@@ -117,128 +110,6 @@ gouraudhdl::~gouraudhdl()
 
 }
 
-//<<<<<<< HEAD
-///* This is the vertex shader for a uniform material. The vertex and normal are in world coordinates, and
-// * you have write access to the varying array which is used to pass data to the fragment shader. Don't
-// * forget that this data is interpolated before it reaches the fragment shader.
-// */
-//vec3f gouraudhdl::shade_vertex(canvashdl *canvas, vec3f vertex, vec3f normal, vector<float> &varying) const
-//{
-//	/* DONE Assignment 3: For flat and gouraud shading, just return the color you passed through the varying array.
-//	 * The final color is calculated in the vertex shader and passed to the fragment shader.
-//	 */
-//
-//	const vector<lighthdl*>* lights;
-//	canvas -> get_uniform("lights", lights);
-//	vec3f a = vec3f(0.0, 0.0, 0.0);
-//	vec3f d = vec3f(0.0, 0.0, 0.0);
-//	vec3f s = vec3f(0.0, 0.0, 0.0);
-//
-//	for(int i=0; (lights != NULL) && (i<lights->size()); i++){
-//
-//		(*lights)[i] -> shade(a, d, s, vertex, normal, shininess);
-//
-//	}
-//
-//	vec3f color = a*this->ambient +d*this ->diffuse +s*this->specular + this->emission ;
-//
-//	varying.push_back(color.data[0]);
-//	varying.push_back(color.data[1]);
-//	varying.push_back(color.data[2]);
-//
-//
-//	vec4f point = canvas -> matrices[canvas -> projection_matrix] * homogenize(vertex);
-//	point = point / point.data[3];
-//
-//	return point;
-//}
-//
-//vec3f gouraudhdl::shade_fragment(canvashdl *canvas, vector<float> &varying) const
-//{
-//	/* DONE Assignment 3: For flat and gouraud shading, just return the color you passed through the varying array.
-//	 * The final color is calculated in the vertex shader and passed to the fragment shader.
-//	 */
-//	vec3f color = clamp(vec3f(varying[0], varying[1], varying[2]), 0.0f, 1.0f);
-//	return color;
-//}
-//
-//materialhdl *gouraudhdl::clone() const
-//{
-//	gouraudhdl *result = new gouraudhdl();
-//	result->type = type;
-//	result->emission = emission;
-//	result->ambient = ambient;
-//	result->diffuse = diffuse;
-//	result->specular = specular;
-//	result->shininess = shininess;
-//	return result;
-//}
-//
-//phonghdl::phonghdl()
-//{
-//	type = "phong";
-//	emission = vec3f(0.0, 0.0, 0.0);
-//	ambient = vec3f(0.1, 0.1, 0.1);
-//	diffuse = vec3f(1.0, 1.0, 1.0);
-//	specular = vec3f(1.0, 1.0, 1.0);
-//	shininess = 1.0;
-//}
-//
-//phonghdl::~phonghdl()
-//{
-//
-//}
-//
-///* This is the vertex shader for a uniform material. The vertex and normal are in world coordinates, and
-// * you have write access to the varying array which is used to pass data to the fragment shader. Don't
-// * forget that this data is interpolated before it reaches the fragment shader.
-// */
-//vec3f phonghdl::shade_vertex(canvashdl *canvas, vec3f vertex, vec3f normal, vector<float> &varying) const
-//{
-//	/* DONE Assignment 3: Implement phong shading, doing the same thing that you did to implement the gouraud
-//	 * and flat. The difference is that the normals have been interpolated and passed into the fragment shader
-//	 * instead of the color. The final color is calculated in the fragment shader.
-//	 */
-//	vec4f point = canvas -> matrices[canvas -> projection_matrix] * homogenize(vertex);
-//	point = point / point.data[3];
-//
-//	varying.push_back(vertex[0]);
-//	varying.push_back(vertex[1]);
-//	varying.push_back(vertex[2]);
-//	varying.push_back(normal[0]);
-//	varying.push_back(normal[1]);
-//	varying.push_back(normal[2]);
-//
-//	return point;
-//}
-//
-//vec3f phonghdl::shade_fragment(canvashdl *canvas, vector<float> &varying) const
-//{
-//	/* DONE Assignment 3: Implement phong shading, doing the same thing that you did to implement the gouraud
-//	 * and flat. The difference is that the normals have been interpolated and passed into the fragment shader
-//	 * instead of the color. The final color is calculated in the fragment shader.
-//	 */
-//	const vector<lighthdl*>* lights;
-//	canvas -> get_uniform("lights", lights);
-//	vec3f a = vec3f(0.0, 0.0, 0.0);
-//	vec3f d = vec3f(0.0, 0.0, 0.0);
-//	vec3f s = vec3f(0.0, 0.0, 0.0);
-//
-//
-//	vec3f vertex = vec3f(varying[0], varying[1], varying[2]);
-//	vec3f normal = vec3f(varying[3], varying[4], varying[5]);
-//
-//	for(int i=0; (lights != NULL) && (i<lights->size()); i++){
-//
-//		(*lights)[i] -> shade(a, d, s, vertex, normal, shininess);
-//
-//	}
-//
-//	vec3f color = a*this->ambient +d*this ->diffuse +s*this->specular + this->emission ;
-//	color = clamp(color, 0.0f, 1.0f);
-//
-//	return color;
-//=======
 void gouraudhdl::apply(const vector<lighthdl*> &lights)
 {
 	// TODO Assignment 4: Apply the shader program and pass it the necessary uniform values
@@ -272,6 +143,12 @@ phonghdl::phonghdl()
 		 * this class. So you only have to initialize them once when the first instance of
 		 * the class is created.
 		 */
+		vertex = load_shader_file(working_directory + "res/phong.vx", GL_VERTEX_SHADER);
+		fragment = load_shader_file(working_directory + "res/phong.ft", GL_FRAGMENT_SHADER);
+		program = glCreateProgram();
+		glAttachShader(program, vertex);
+		glAttachShader(program, fragment);
+		glLinkProgram(program);
 	}
 }
 
@@ -300,13 +177,6 @@ materialhdl *phonghdl::clone() const
 customhdl::customhdl()
 {
 	type = "custom";
-//<<<<<<< HEAD
-//	emission = vec3f(0.0, 0.0, 0.0);
-//	ambient = vec3f(0.1, 0.1, 0.1);
-//	diffuse = vec3f(1.0, 1.0, 1.0);
-//	specular = vec3f(1.0, 1.0, 1.0);
-//	shininess = 1.0;
-//=======
 
 	if (vertex == 0 && fragment == 0 && program == 0)
 	{
@@ -315,6 +185,12 @@ customhdl::customhdl()
 		 * this class. So you only have to initialize them once when the first instance of
 		 * the class is created.
 		 */
+		vertex = load_shader_file(working_directory + "res/custom.vx", GL_VERTEX_SHADER);
+		fragment = load_shader_file(working_directory + "res/custom.ft", GL_FRAGMENT_SHADER);
+		program = glCreateProgram();
+		glAttachShader(program, vertex);
+		glAttachShader(program, fragment);
+		glLinkProgram(program);
 	}
 }
 
