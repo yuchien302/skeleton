521c2c27c61ca3f65460de4e4aacebf613719617
yuchien <yc2257@cornell.edu>
2015-11-22 23:12:40 -0500

    Rewrite shade_point

--- res/light.glsl
+++ res/light.glsl
@@ -39,29 +39,26 @@ struct point
 };
 
 void shade_point(point light, inout vec3 ambient, inout vec3 diffuse, inout vec3 specular, vec3 vertex, vec3 normal, float shininess)
-{
-    vec3 light_direction = light.position - vertex;
-    float light_distance = length(light_direction);
-    light_direction /= light_distance;
-    
-    vec3 eye_direction = -vertex;
-    float eye_distance = length(eye_direction);
-    eye_direction /= eye_distance;
-    
-    float att = 1.0/(light.attenuation[0] + light.attenuation[1]*light_distance + light.attenuation[2]*light_distance*light_distance);
-    
-    vec3 half_vector = normalize(light_direction + eye_direction);
-    
-    float normal_dot_light_direction = max(0.0, dot(normal, light_direction));
-    float normal_dot_half_vector = max(0.0, dot(normal, half_vector));
-    
-    float power_factor = 0.0;
-    if (normal_dot_light_direction > 0.0)
-        power_factor = pow(normal_dot_half_vector, shininess);
-    
-    ambient += light.ambient*att;
-    diffuse += light.diffuse*normal_dot_light_direction*att;
-    specular += light.specular*power_factor*att;
+{    
+    vec3 toLight = light.position - vertex;
+	float d = length(toLight);
+	toLight = toLight/d;
+	vec3 toEye = normalize(-vertex);
+	normal = normalize(normal);
+	float diff_cosine = max(0.0, dot(normal, toLight));
+	float spec_base = max(0.0, dot(normal, normalize(toLight+toEye)));
+	float spec_pf;
+
+	if(diff_cosine == 0.0)
+		spec_pf = 0.0;
+	else
+		spec_pf = pow(spec_base, shininess);
+
+	float decay = 1.0 / (light.attenuation[0] + light.attenuation[1]*d + light.attenuation[2]*d*d);
+
+	ambient += light.ambient * decay;
+	diffuse += light.diffuse * diff_cosine * decay;
+	specular += light.specular * spec_pf * decay;
 }
 
 struct spot
